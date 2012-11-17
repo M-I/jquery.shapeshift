46a6ba3ca622cb563c842c8155afeb95901a907f
Scott W. Elwood <McPants32@gmail.com>
2012-11-17 09:06:52 -0800

    Updated js

--- demo.js
+++ demo.js
@@ -1,15 +1,7 @@
-function getRandomColor() {
-  var letters = 'ABCDEF'.split('');
-  var color = '#';
-  for (var i=0;i<6;i++) {
-    color += letters[Math.round(Math.random() * 5)];
-  }
-  return color;
-}
+var $containers = $(".container");
 
 function renderChildren(placekitten) {
   // Lets generate some child divs
-  var $containers = $(".container");
   $containers.children().remove();
   $containers.each(function() {
     for(i=0;i<15;i++) {
@@ -17,7 +9,7 @@ function renderChildren(placekitten) {
           height = Math.floor(Math.random() * 200) + 100,
           width = 200;
       if(placekitten) {
-        var background = 'http://www.placekitten.com/'+width+'/'+height
+        var background = 'url("http://www.placekitten.com/'+width+'/'+height+'")';
       } else {
         var background = getRandomColor(),
             $img = $('<img src="http://placehold.it/'+width+'x'+Math.floor(height / 2)+'" />');
@@ -35,28 +27,38 @@ $(".filter").on("click", function(e) {
   e.preventDefault();
 
   if($(this).hasClass("no-drag-animate")) {
-    $container.shapeshift({animatedOnDrag: false});
+    $containers.shapeshift({animatedOnDrag: false});
   }
   if($(this).hasClass("no-drag")) {
-    $container.shapeshift({draggable: false});
+    $containers.shapeshift({draggable: false});
   }
   if($(this).hasClass("drag")) {
-    $container.shapeshift();
+    $containers.shapeshift();
   }
   if($(this).hasClass("hide")) {
-    var $objects = $container.children().filter(":visible"),
+    var $objects = $containers.children().filter(":visible"),
         random = Math.round(Math.random()*7);
     $objects.eq(random).hide();
-    $(".container").shapeshift();
+    $containers.shapeshift();
   }
   if($(this).hasClass("show")) {
-    var $objects = $container.children().filter(":hidden"),
+    var $objects = $containers.children().filter(":hidden"),
         random = Math.round(Math.random()*3);
     $objects.eq(random).show();
-    $(".container").shapeshift();
+    $containers.shapeshift();
   }
   if($(this).hasClass("placekittens")) {
     renderChildren(true);
+    $containers.shapeshift();
   }
 })
-$(".container").shapeshift();
+$containers.shapeshift();
+
+function getRandomColor() {
+  var letters = 'ABCDEF'.split('');
+  var color = '#';
+  for (var i=0;i<6;i++) {
+    color += letters[Math.round(Math.random() * 5)];
+  }
+  return color;
+}
