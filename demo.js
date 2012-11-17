83270e5e49bb7283fe191fb2b54808c1a40688b5
Scott W. Elwood <McPants32@gmail.com>
2012-11-17 07:34:44 -0800

    Updated JS

--- demo.js
+++ demo.js
@@ -8,20 +8,22 @@ function getRandomColor() {
 }
 
 // Lets generate some child divs
-var $container = $("#container");
-for(i=0;i<45;i++) {
-  var $element = $("<div></div>"),
-      height = Math.floor(Math.random() * 450) + 70,
-      width = 300,
-      color = getRandomColor(),
-      $img = $('<img src="http://placehold.it/'+width+'x'+Math.floor(height / 2)+'" />');
-  $element.css({
-    height: height,
-    background: color
-  });
-  $container.append($element);
-  $element.append($img);
-}
+var $container = $(".container");
+$container.each(function() {
+  for(i=0;i<20;i++) {
+    var $element = $("<div></div>"),
+        height = Math.floor(Math.random() * 200) + 100,
+        width = 200,
+        color = getRandomColor(),
+        $img = $('<img src="http://placehold.it/'+width+'x'+Math.floor(height / 2)+'" />');
+    $element.css({
+      height: height,
+      background: color
+    });
+    $(this).append($element);
+    $element.append($img);
+  }
+})
 
 // And now we can shapeshift!
 $(".filter").on("click", function(e) {
@@ -40,35 +42,37 @@ $(".filter").on("click", function(e) {
     var $objects = $container.children().filter(":visible"),
         random = Math.round(Math.random()*7);
     $objects.eq(random).hide();
-    $("#container").shapeshift();
+    $(".container").shapeshift();
   }
   if($(this).hasClass("show")) {
     var $objects = $container.children().filter(":hidden"),
         random = Math.round(Math.random()*3);
     $objects.eq(random).show();
-    $("#container").shapeshift();
+    $(".container").shapeshift();
   }
   if($(this).hasClass("placekittens")) {
-    var $objects = $container.children().filter(":visible");
-    $objects.each(function(i) {
-      var width = $(this).outerWidth(),
-          height = $(this).outerHeight(),
-          $img = $('<img src="http://www.placekitten.com/'+width+"/"+height+'" width="'+width+'" height="'+height+'" />');
+    $container.each(function() {
+      var $objects = $(this).children().filter(":visible");
+      $objects.each(function(i) {
+        var width = $(this).outerWidth(),
+            height = $(this).outerHeight(),
+            $img = $('<img src="http://www.placekitten.com/'+width+"/"+height+'" width="'+width+'" height="'+height+'" />');
 
-      $img.css({
-        left: $(this).position().left,
-        top: $(this).position().top,
-        position: "absolute"
+        $img.css({
+          left: $(this).position().left,
+          top: $(this).position().top,
+          position: "absolute"
+        })
+        $container.append($img);
+        $(this).remove();
+
+        if(i === $objects.length - 1) {
+          setTimeout(function() {
+            $(".container").shapeshift();
+          }, 300);
+        }
       })
-      $(container).append($img);
-      $(this).remove();
-
-      if(i === $objects.length - 1) {
-        setTimeout(function() {
-          $("#container").shapeshift();
-        }, 300);
-      }
     })
   }
 })
-$("#container").shapeshift();
+$(".container").shapeshift();
